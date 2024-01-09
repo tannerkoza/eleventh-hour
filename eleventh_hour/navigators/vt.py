3ecdd25cf8d182a50134a497044f3c28dbe84fb1
Tanner Koza <kozatanner@gmail.com>
2024-01-09 10:19:10 -0600

    Refactor VDFLL logging

--- eleventh_hour/navigators/vt.py
+++ eleventh_hour/navigators/vt.py
@@ -1,6 +1,5 @@
 import numpy as np
 import navsim as ns
-import pandas as pd
 import navtools as nt
 import scipy.linalg as linalg
 
@@ -14,15 +13,19 @@ from navsim.error_models.clock import NavigationClock
 @njit(cache=True)
 def compute_prange_residual_var(cn0: np.ndarray, T: float, chip_length: float):
     cn0 = 10 ** (cn0 / 10)
-    return chip_length**2 * (1 / (2 * T**2 * cn0**2) + 1 / (4 * T * cn0))
+    var = chip_length**2 * (1 / (2 * T**2 * cn0**2) + 1 / (4 * T * cn0))
+
+    return var
 
 
 @njit(cache=True)
 def compute_prange_rate_residual_var(cn0: np.ndarray, T: float, wavelength: float):
     cn0 = 10 ** (cn0 / 10)
-    return (wavelength / (np.pi * T)) ** 2 * (
+    var = (wavelength / (np.pi * T)) ** 2 * (
         2 / ((T) ** 2 * cn0**2) + 2 / ((T) * cn0)
     )
+
+    return var
 
 
 @dataclass(frozen=True)
@@ -46,15 +49,31 @@ class VDFLLConfiguration:
 
 
 @dataclass(frozen=True)
+class VDFLLReceiverStates:
+    pos: np.ndarray
+    vel: np.ndarray
+    clock_bias: np.ndarray
+    clock_drift: np.ndarray
+
+
+@dataclass(frozen=True)
+class VDFLLChannelErrors:
+    chip: defaultdict
+    frequency: defaultdict
+    prange: defaultdict
+    prange_rate: defaultdict
+
+
+@dataclass(frozen=True)
 class VDFLLCorrelators:
-    ip: np.ndarray
-    qp: np.ndarray
-    ie: np.ndarray
-    qe: np.ndarray
-    il: np.ndarray
-    ql: np.ndarray
-    sub_ip: np.ndarray
-    sub_qp: np.ndarray
+    ip: defaultdict
+    qp: defaultdict
+    ie: defaultdict
+    qe: defaultdict
+    il: defaultdict
+    ql: defaultdict
+    sub_ip: defaultdict
+    sub_qp: defaultdict
 
 
 class VDFLL:
@@ -96,118 +115,74 @@ class VDFLL:
 
         # logging
         self.__rx_states_log = []
-        self.__code_error_log = defaultdict(lambda: defaultdict(lambda: []))
-        self.__ferror_log = defaultdict(lambda: defaultdict(lambda: []))
-        self.__prange_error_log = defaultdict(lambda: defaultdict(lambda: []))
-        self.__prange_rate_error_log = defaultdict(lambda: defaultdict(lambda: []))
 
-        self.__correlator_log = []
+        self.__chip_error_log = defaultdict(lambda: [])
+        self.__ferror_log = defaultdict(lambda: [])
+        self.__prange_error_log = defaultdict(lambda: [])
+        self.__prange_rate_error_log = defaultdict(lambda: [])
+
+        self.__ip_log = defaultdict(lambda: [])
+        self.__qp_log = defaultdict(lambda: [])
+        self.__sub_ip_log = defaultdict(lambda: [])
+        self.__sub_qp_log = defaultdict(lambda: [])
+
+        self.__ie_log = defaultdict(lambda: [])
+        self.__qe_log = defaultdict(lambda: [])
+
+        self.__il_log = defaultdict(lambda: [])
+        self.__ql_log = defaultdict(lambda: [])
 
     # properties
     @property
     def rx_states(self):
-        rx_states = pd.DataFrame(
-            self.__rx_states_log,
-            columns=[
-                "x_pos",
-                "x_vel",
-                "y_pos",
-                "y_vel",
-                "z_pos",
-                "z_vel",
-                "clock_bias",
-                "clock_drfit",
-            ],
+        rx_states_log = np.array(self.__rx_states_log)
+        rx_states = VDFLLReceiverStates(
+            pos=rx_states_log[:, :6:2],
+            vel=rx_states_log[:, 1:7:2],
+            clock_bias=rx_states_log[:, 6],
+            clock_drift=rx_states_log[:, 7],
         )
 
         return rx_states
 
     @property
-    def code_errors(self):
-        code_errors = pd.DataFrame(self.__code_error_log)
+    def channel_errors(self):
+        channel_errors = VDFLLChannelErrors(
+            chip=self.__chip_error_log,
+            frequency=self.__ferror_log,
+            prange=self.__prange_error_log,
+            prange_rate=self.__prange_rate_error_log,
+        )
 
-        return code_errors
-
-    @property
-    def ferrors(self):
-        ferrors = pd.DataFrame(self.__ferror_log)
-
-        return ferrors
-
-    @property
-    def prange_errors(self):
-        prange_errors = pd.DataFrame(self.__prange_error_log)
-
-        return prange_errors
-
-    @property
-    def prange_rate_errors(self):
-        prange_rate_errors = pd.DataFrame(self.__prange_rate_error_log)
-
-        return prange_rate_errors
+        return channel_errors
 
     @property
     def correlators(self):
-        return np.array(self.__correlator_log)
+        correlators = VDFLLCorrelators(
+            ip=self.__ip_log,
+            qp=self.__qp_log,
+            ie=self.__ie_log,
+            qe=self.__qe_log,
+            il=self.__il_log,
+            ql=self.__ql_log,
+            sub_ip=self.__sub_ip_log,
+            sub_qp=self.__sub_qp_log,
+        )
+        return correlators
 
     # public
     def time_update(self, T: float):
         self.T = T
 
+        # state transition & process covariance
         self.__compute_A()
         self.__compute_Q()
 
         self.rx_state = self.A @ self.rx_state
         self.P = self.A @ self.P @ self.A.T + self.Q
 
-    def loop_closure(self):
-        I = np.eye(self.rx_state.size)
-        self.__compute_H()
-        self.__compute_R()
-
-        if self.__is_covariance_not_ss:
-            self.__compute_ss_covariance()
-            self.__is_covariance_not_ss = False
-
-        code_error, ferror = self.__discriminate()
-        prange_error = code_error * self.chip_length
-        prange_rate_error = ferror * -self.wavelength
-        z = np.append(prange_error, prange_rate_error)
-
-        S = self.H @ self.P @ self.H.T + self.R
-        norm_z = np.abs(z / np.sqrt(np.diag(S)))
-
-        if np.any(norm_z) > self.norm_innovation_thresh:
-            return
-
-        K = self.P @ self.H.T @ np.linalg.inv(S)
-        self.rx_state += K @ z
-        self.P = (I - K @ self.H) @ self.P @ (I - K @ self.H).T + K @ self.R @ K.T
-
-        # logging
-        self.__rx_states_log.append(self.rx_state)
-        self.__log_by_emitter(data=code_error, log=self.__code_error_log)
-        self.__log_by_emitter(data=ferror, log=self.__ferror_log)
-        self.__log_by_emitter(data=prange_error, log=self.__prange_error_log)
-        self.__log_by_emitter(data=prange_rate_error, log=self.__prange_rate_error_log)
-
-    def update_correlator_buffers(
-        self,
-        prompt: ns.CorrelatorOutputs,
-        early: ns.CorrelatorOutputs,
-        late: ns.CorrelatorOutputs,
-    ):
-        # TODO: adapt this method to handle len(buff) > 1
-        self.ip = prompt.inphase
-        self.qp = prompt.quadrature
-        self.ie = early.inphase
-        self.qe = early.quadrature
-        self.il = late.inphase
-        self.ql = late.quadrature
-        self.sub_ip = prompt.subinphase
-        self.sub_qp = prompt.subquadrature
-
     def predict_observables(self, emitter_states: dict):
+        # extract current state
         rx_pos = self.rx_state[:6:2]
         rx_vel = self.rx_state[1:7:2]
         rx_clock_bias = self.rx_state[6]
@@ -218,42 +193,94 @@ class VDFLL:
         unit_vectors = []
         range_rates = []
 
+        # geometric range & range rate determination
         for emitter_state in emitter_states.values():
-            range, unit_vector = nt.compute_range_and_unit_vector(
+            grange, unit_vector = nt.compute_range_and_unit_vector(
                 rx_pos=rx_pos, emitter_pos=emitter_state.pos
             )
-            range_rate = nt.compute_range_rate(
+            grange_rate = nt.compute_range_rate(
                 rx_vel=rx_vel, emitter_vel=emitter_state.vel, unit_vector=unit_vector
             )
 
             constellations.append(emitter_state.constellation)
-            ranges.append(range)
+            ranges.append(grange)
             unit_vectors.append(unit_vector)
-            range_rates.append(range_rate)
+            range_rates.append(grange_rate)
 
-        # TODO: add group delay from emitter states
+        # observable prediction
+        # TODO: add group delay/drift from emitter states
         pranges = np.array(ranges) + rx_clock_bias
         prange_rates = np.array(range_rates) + rx_clock_drift
 
-        self.__emitter_states = emitter_states
-        self.nchannels = len(constellations)
-        self.unit_vectors = np.array(unit_vectors).T
+        # update state
         self.__update_cycle_lengths(
             constellations=constellations, prange_rates=prange_rates
         )
+        self.__nchannels = len(constellations)
+        self.__emitter_states = emitter_states
+        self.__unit_vectors = np.array(unit_vectors).T
 
         return pranges, prange_rates
 
-    @staticmethod
-    def pad_log(log: list):
-        epochs = len(log)
-        max_nemitters = len(max(log, key=len))
-        padded_log = np.full((epochs, max_nemitters), np.nan)
+    def update_correlator_buffers(
+        self,
+        prompt: ns.CorrelatorOutputs,
+        early: ns.CorrelatorOutputs,
+        late: ns.CorrelatorOutputs,
+    ):
+        # TODO: adapt this method to handle len(buff) > 1
+        self.__ip = prompt.inphase
+        self.__qp = prompt.quadrature
+        self.__sub_ip = prompt.subinphase
+        self.__sub_qp = prompt.subquadrature
+        self.__log_by_emitter(data=self.__ip, log=self.__ip_log)
+        self.__log_by_emitter(data=self.__qp, log=self.__qp_log)
+        self.__log_by_emitter(data=self.__sub_ip.T, log=self.__sub_ip_log)
+        self.__log_by_emitter(data=self.__sub_qp.T, log=self.__sub_qp_log)
 
-        for epoch, emitters in enumerate(log):
-            padded_log[epoch][0 : len(emitters)] = emitters
+        self.__ie = early.inphase
+        self.__qe = early.quadrature
+        self.__log_by_emitter(data=self.__ie, log=self.__ie_log)
+        self.__log_by_emitter(data=self.__qe, log=self.__qe_log)
 
-        return padded_log
+        self.__il = late.inphase
+        self.__ql = late.quadrature
+        self.__log_by_emitter(data=self.__il, log=self.__il_log)
+        self.__log_by_emitter(data=self.__ql, log=self.__ql_log)
+
+    def loop_closure(self):
+        # observation & measurement covariance
+        self.__compute_H()
+        self.__compute_R()
+        I = np.eye(self.rx_state.size)
+
+        if self.__is_covariance_not_ss:
+            self.__compute_ss_covariance()  # allows for quick convergence
+            self.__is_covariance_not_ss = False
+
+        # obtain measurements
+        chip_error, ferror = self.__discriminate()
+        prange_error = chip_error * self.chip_length
+        prange_rate_error = ferror * -self.wavelength
+        z = np.append(prange_error, prange_rate_error)
+
+        S = self.H @ self.P @ self.H.T + self.R  # innovation covariance for fde
+
+        norm_z = np.abs(z / np.sqrt(np.diag(S)))  # fault detection & exclusion
+        if np.any(norm_z) > self.norm_innovation_thresh:
+            return
+
+        # state & covariance update
+        K = self.P @ self.H.T @ np.linalg.inv(S)
+        self.rx_state += K @ z
+        self.P = (I - K @ self.H) @ self.P @ (I - K @ self.H).T + K @ self.R @ K.T
+
+        # logging
+        self.__rx_states_log.append(self.rx_state)
+        self.__log_by_emitter(data=chip_error, log=self.__chip_error_log)
+        self.__log_by_emitter(data=ferror, log=self.__ferror_log)
+        self.__log_by_emitter(data=prange_error, log=self.__prange_error_log)
+        self.__log_by_emitter(data=prange_rate_error, log=self.__prange_rate_error_log)
 
     # private
     def __compute_A(self):
@@ -283,12 +310,12 @@ class VDFLL:
         self.Q = linalg.block_diag(xyz_Q, xyz_Q, xyz_Q, clock_Q)
 
     def __compute_H(self):
-        ux = self.unit_vectors[0]
-        uy = self.unit_vectors[1]
-        uz = self.unit_vectors[2]
+        ux = self.__unit_vectors[0]
+        uy = self.__unit_vectors[1]
+        uz = self.__unit_vectors[2]
 
-        range_states_H = np.zeros((self.rx_state.size, self.nchannels))
-        rate_states_H = np.zeros((self.rx_state.size, self.nchannels))
+        range_states_H = np.zeros((self.rx_state.size, self.__nchannels))
+        rate_states_H = np.zeros((self.rx_state.size, self.__nchannels))
 
         range_states_H[0] = ux
         range_states_H[2] = uy
@@ -310,46 +337,8 @@ class VDFLL:
             cn0=self.cn0, T=self.T, wavelength=self.wavelength
         )
         residual_vars = np.append(prange_residual_var, prange_rate_residual_var)
+
         self.R = np.diag(residual_vars)
-
-    def __discriminate(self):
-        # code discriminator
-        e_envelope = np.sqrt(self.ie**2 + self.qe**2)
-        l_envelope = np.sqrt(self.il**2 + self.ql**2)
-        code_error = (
-            (1 - self.tap_spacing)
-            * (e_envelope - l_envelope)
-            / (e_envelope + l_envelope)
-        )
-
-        # subcorrelators
-        split_ip = np.array_split(self.sub_ip, 2)
-        split_qp = np.array_split(self.sub_qp, 2)
-
-        first_ip = np.mean(split_ip[0], axis=0)
-        first_qp = np.mean(split_qp[0], axis=0)
-        last_ip = np.mean(split_ip[1], axis=0)
-        last_qp = np.mean(split_qp[1], axis=0)
-
-        # frequency discriminator
-        cross = first_ip * last_qp - last_ip * first_qp
-        dot = first_ip * last_ip + first_qp * last_qp
-        ferror = np.arctan2(cross, dot) / (np.pi * self.T)
-
-        # logging
-        correlators = VDFLLCorrelators(
-            ip=self.ip,
-            qp=self.qp,
-            ie=self.ie,
-            qe=self.qe,
-            il=self.il,
-            ql=self.ql,
-            sub_ip=self.sub_ip,
-            sub_qp=self.sub_qp,
-        )
-        self.__correlator_log.append(correlators)
-
-        return code_error, ferror
 
     def __update_cycle_lengths(self, constellations: list, prange_rates: np.ndarray):
         chip_length = []
@@ -359,7 +348,7 @@ class VDFLL:
             properties = self.__signal_properties.get(constellation.casefold())
 
             fcarrier = properties.fcarrier
-            fchip = properties.fchip_data  # ! assumes tracking data channel
+            fchip = properties.fchip_data  # ! assumes tracking data channel !
 
             fratio = fchip / fcarrier
             carrier_doppler = -prange_rate * (fcarrier / SPEED_OF_LIGHT)
@@ -386,6 +375,32 @@ class VDFLL:
 
             delta_diag_P = np.diag(previous_P - self.P)
 
+    def __discriminate(self):
+        # code discriminator
+        e_envelope = np.sqrt(self.__ie**2 + self.__qe**2)
+        l_envelope = np.sqrt(self.__il**2 + self.__ql**2)
+        chip_error = (
+            (1 - self.tap_spacing)
+            * (e_envelope - l_envelope)
+            / (e_envelope + l_envelope)
+        )
+
+        # subcorrelators
+        split_ip = np.array_split(self.__sub_ip, 2)
+        split_qp = np.array_split(self.__sub_qp, 2)
+
+        first_ip = np.mean(split_ip[0], axis=0)
+        first_qp = np.mean(split_qp[0], axis=0)
+        last_ip = np.mean(split_ip[1], axis=0)
+        last_qp = np.mean(split_qp[1], axis=0)
+
+        # frequency discriminator
+        cross = first_ip * last_qp - last_ip * first_qp
+        dot = first_ip * last_ip + first_qp * last_qp
+        ferror = np.arctan2(cross, dot) / (np.pi * self.T)
+
+        return chip_error, ferror
+
     def __log_by_emitter(self, data: np.ndarray, log: defaultdict):
         for idx, emitter in enumerate(self.__emitter_states.values()):
-            log[emitter.constellation][emitter.id].append(data[idx])
+            log[(emitter.constellation, emitter.id)].append(data[idx])
