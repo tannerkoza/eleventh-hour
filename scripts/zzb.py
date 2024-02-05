36cfb90ca165aa5ce2937b3c23682a5459dc2bd1
Tanner Koza <kozatanner@gmail.com>
2024-02-05 15:19:19 -0600

    Update rmsbw calculation

--- scripts/zzb.py
+++ scripts/zzb.py
@@ -28,9 +28,13 @@ def compute_H(uv: np.ndarray):
     H = np.zeros((8, nchannels))
 
     H[0] = ux
+    H[1] = ux
     H[2] = uy
+    H[3] = uy
     H[4] = uz
+    H[5] = uz
     H[6] = np.ones_like(ux)
+    H[7] = np.ones_like(ux)
 
     return H
 
@@ -56,29 +60,35 @@ for js in JS:
         if constellation in ["gps"]:
             properties.js = js
 
-        meas_sim.generate_truth(rx_pos=rx_pos[0])
-        meas_sim.simulate()
+    meas_sim.generate_truth(rx_pos=rx_pos[0])
+    meas_sim.simulate()
 
-        emitters = meas_sim.emitter_states.truth
-        observables = meas_sim.observables
+    emitters = meas_sim.emitter_states.truth
+    observables = meas_sim.observables
 
-        snr = np.array(
-            [(10 ** (ob.cn0 / 10)) / (1 / TSIM) for ob in observables[0].values()]
-        )
-        uv = np.array([em.uv for em in emitters[0].values()]).T
+    snr = np.array(
+        [(10 ** (ob.cn0 / 10)) / (1 / TSIM) for ob in observables[0].values()]
+    )
+    uv = np.array([em.uv for em in emitters[0].values()]).T
 
-        f = np.linspace(-0.5 * (1 / TSIM), 0.5 * (1 / TSIM), int(1e6 + 1))
-        rmsbw = np.sum(f**2 * np.sinc(np.pi * TSIM * f))  # THIS IS SO WRONG
-        H = compute_H(uv)
-        I = H @ (rmsbw * np.diag(snr)) @ H.T
+    f = np.linspace(-0.5 * (1 / TSIM), 0.5 * (1 / TSIM), int(1e6 + 1))
+    G = 1.023e6 * (np.sin((np.pi * f) / 1.023e6) ** 2 / (np.pi * f) ** 2)
+    rmsbw = np.sum(f**2 * G / np.max(G))  # THIS IS SO WRONG
+    H = compute_H(uv)
+    I = H @ (rmsbw * np.diag(snr)) @ H.T
 
-        if js == 0.0:
-            R = np.linalg.pinv(I)
+    if js == 0.0:
+        R = np.linalg.pinv(I)
 
-        Q = 0.5 * erfc(np.sqrt(np.sum(snr)) / np.sqrt(2))
-        gamma = gammainc(1.5, np.sum(snr / 2))
+    I = np.eye(8)
+    R = np.linalg.pinv(I) * 100
 
-        zzb = R * 2 * Q + np.linalg.pinv(I) * gamma
-        zzbs.append(zzb)
+    Q = 0.5 * erfc(np.sqrt(np.sum(snr)) / np.sqrt(2))
+    gamma = gammainc(1.5, np.sum(snr / 4))
 
-        meas_sim.clear_observables()
+    zzb = R * 2 * Q + np.linalg.pinv(I) * gamma
+    zzbs.append(zzb)
+
+    meas_sim.clear_observables()
+
+print()
