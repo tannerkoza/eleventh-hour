4f8d8e054224f15841a39dea75cb7950131f9e93
Tanner Koza <kozatanner@gmail.com>
2024-02-05 17:51:50 -0600

    Update for Sam

--- scripts/zzb.py
+++ scripts/zzb.py
@@ -2,13 +2,14 @@ import numpy as np
 import navsim as ns
 
 from pathlib import Path
-from datetime import datetime, timedelta
+from datetime import datetime
 from scipy.special import erfc, gammainc
+from navtools.constants import SPEED_OF_LIGHT
 from eleventh_hour.trajectories import prepare_trajectories
-from navsim.simulations.measurement import MeasurementSimulation
 
 DURATION = 300
 TSIM = 0.02
+FE_BW = 2 * (2 / 1.023e6)
 JS = np.arange(0, 45, 5, dtype=float)
 INTITAL_TIME = datetime(year=2023, month=12, day=4, second=18)
 TRAJECTORY = "daytona_500_sdx_1s_loop"
@@ -25,16 +26,16 @@ def compute_H(uv: np.ndarray):
     uy = uv[1]
     uz = uv[2]
 
-    H = np.zeros((8, nchannels))
+    H = np.zeros((3, nchannels))
 
     H[0] = ux
     H[1] = ux
     H[2] = uy
-    H[3] = uy
-    H[4] = uz
-    H[5] = uz
-    H[6] = np.ones_like(ux)
-    H[7] = np.ones_like(ux)
+    # H[3] = uy
+    # H[4] = uz
+    # H[5] = uz
+    # H[6] = np.ones_like(ux)
+    # H[7] = np.ones_like(ux)
 
     return H
 
@@ -71,17 +72,19 @@ for js in JS:
     )
     uv = np.array([em.uv for em in emitters[0].values()]).T
 
-    f = np.linspace(-0.5 * (1 / TSIM), 0.5 * (1 / TSIM), int(1e6 + 1))
+    f = np.linspace(-FE_BW / 2, FE_BW / 2, 10000)
     G = 1.023e6 * (np.sin((np.pi * f) / 1.023e6) ** 2 / (np.pi * f) ** 2)
-    rmsbw = np.sum(f**2 * G / np.max(G))  # THIS IS SO WRONG
-    H = compute_H(uv)
-    I = H @ (rmsbw * np.diag(snr)) @ H.T
+    G = G / np.sum(G)
+    rmsbw = np.sqrt(np.sum(f**2 * G))  # THIS IS SO WRONG
+    H = compute_H(uv).T
+    I = H.T @ (rmsbw * np.diag(snr)) @ H
 
     if js == 0.0:
         R = np.linalg.pinv(I)
+        # R = np.eye(3)
 
-    I = np.eye(8)
-    R = np.linalg.pinv(I) * 100
+    # I = np.eye(8)
+    # R = np.linalg.pinv(I) * 100
 
     Q = 0.5 * erfc(np.sqrt(np.sum(snr)) / np.sqrt(2))
     gamma = gammainc(1.5, np.sum(snr / 4))
