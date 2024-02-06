aa85d940d106d883ff7a58b5a1b577d60cf7b5d4
Tanner Koza <kozatanner@gmail.com>
2024-02-06 15:11:17 -0600

    Update for Sam

--- scripts/zzb.py
+++ scripts/zzb.py
@@ -4,12 +4,14 @@ import navsim as ns
 from pathlib import Path
 from datetime import datetime
 from scipy.special import erfc, gammainc
+from navtools.conversions import cn02snr
 from navtools.constants import SPEED_OF_LIGHT
 from eleventh_hour.trajectories import prepare_trajectories
 
 DURATION = 300
 TSIM = 0.02
-FE_BW = 2 * (2 / 1.023e6)
+FEBW = 100000 * 6.138e6
+FCHIP = 1.023e6
 JS = np.arange(0, 45, 5, dtype=float)
 INTITAL_TIME = datetime(year=2023, month=12, day=4, second=18)
 TRAJECTORY = "daytona_500_sdx_1s_loop"
@@ -29,8 +31,8 @@ def compute_H(uv: np.ndarray):
     H = np.zeros((3, nchannels))
 
     H[0] = ux
-    H[1] = ux
-    H[2] = uy
+    H[1] = uy
+    H[2] = uz
     # H[3] = uy
     # H[4] = uz
     # H[5] = uz
@@ -55,6 +57,8 @@ meas_sim = ns.get_signal_simulation(
 
 zzbs = []
 
+R = np.eye(3)
+
 for js in JS:
     # set current js
     for constellation, properties in conf.constellations.emitters.items():
@@ -67,30 +71,25 @@ for js in JS:
     emitters = meas_sim.emitter_states.truth
     observables = meas_sim.observables
 
-    snr = np.array(
-        [(10 ** (ob.cn0 / 10)) / (1 / TSIM) for ob in observables[0].values()]
-    )
+    # snr = np.array([(10 ** (ob.cn0 / 10)) for ob in observables[0].values()])
+    cn0 = np.array([ob.cn0 for ob in observables[0].values()])
+    snr = 10 ** (cn02snr(cn0, 1 / TSIM) / 10)
+
     uv = np.array([em.uv for em in emitters[0].values()]).T
 
-    f = np.linspace(-FE_BW / 2, FE_BW / 2, 10000)
-    G = 1.023e6 * (np.sin((np.pi * f) / 1.023e6) ** 2 / (np.pi * f) ** 2)
-    G = G / np.sum(G)
-    rmsbw = np.sqrt(np.sum(f**2 * G))  # THIS IS SO WRONG
-    H = compute_H(uv).T
-    I = H.T @ (rmsbw * np.diag(snr)) @ H
+    f = np.linspace(-FEBW / 2, FEBW / 2, 1000000)
+    S = (1 / FCHIP) * np.sinc(np.pi * f * (1 / FCHIP)) ** 2
+    norm_S = S / np.sum(S)
+    msbw = np.sum(f**2 * norm_S)
 
-    if js == 0.0:
-        R = np.linalg.pinv(I)
-        # R = np.eye(3)
-
-    # I = np.eye(8)
-    # R = np.linalg.pinv(I) * 100
+    H = compute_H(uv).T / SPEED_OF_LIGHT
+    I = H.T @ (msbw * np.diag(snr)) @ H
 
     Q = 0.5 * erfc(np.sqrt(np.sum(snr)) / np.sqrt(2))
     gamma = gammainc(1.5, np.sum(snr / 4))
 
-    zzb = R * 2 * Q + np.linalg.pinv(I) * gamma
-    zzbs.append(zzb)
+    zzb = R * 2 * Q + np.linalg.inv(I) * gamma
+    zzbs.append(np.diag(zzb))
 
     meas_sim.clear_observables()
 
